1c34396b325b171393292c7cf8e6ed138f017540
Samridhi Sehgal <sehgalsamridhi05@gmail.com>
2026-07-20 20:09:13 +0530

    Time: 0 ms (100%), Space: 8.9 MB (95.67%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        int i=num1.size()-1;
-        int j=num2.size()-1;
-        int carry=0;
-       string ans= solve(num1,i,num2,j,carry);
-           reverse(ans.begin(),ans.end());
-       return ans;
 
+        string ans = "";
+
+        solve(num1, num1.size() - 1,
+              num2, num2.size() - 1,
+              0, ans);
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
     }
-    string solve(string &num1,int i,string &num2,int j,int carry){
-        if(i<0&&j<0){
-            if(carry!=0){
-                return string(1,carry+'0');
-            }
-            return "";}
-            int n1=(i>=0?num1[i]:'0')-'0';
-            int n2=(j>=0?num2[j]:'0')-'0';
-            int csum=n1+n2+carry;
-            int digit=csum%10;
-            carry=csum/10;
-            string ans="";
-            ans.push_back(digit+'0');
-            ans+=solve(num1,i-1,num2,j-1,carry);
-        
-            return ans;
+
+    void solve(string &num1, int i,
+               string &num2, int j,
+               int carry, string &ans) {
+
+        if (i < 0 && j < 0) {
+            if (carry)
+                ans.push_back(carry + '0');
+            return;
         }
-    
+
+        int n1 = (i >= 0) ? num1[i] - '0' : 0;
+        int n2 = (j >= 0) ? num2[j] - '0' : 0;
+
+        int sum = n1 + n2 + carry;
+
+        ans.push_back((sum % 10) + '0');
+
+        solve(num1, i - 1,
+              num2, j - 1,
+              sum / 10, ans);
+    }
 };
